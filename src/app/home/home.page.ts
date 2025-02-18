a6f8a592f379ac9c0e0bb23bcf368002d206f91e
esthertozzo <tozzo.est@gmail.com>
2025-02-18 11:36:00 -0300

    app Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,4 +10,29 @@ export class HomePage {
 
   constructor() {}
 
+  // atributos
+  n1="";
+  n2="";
+  dolar="";
+  real="5.78";
+  res="";
+  valorEmReal="";
+  calcularMediaRes="";
+  ac="";
+  at="";
+  agh="";
+
+  // metodo
+  somar(){
+    this.res = (parseFloat(this.n1) + parseFloat(this.n2)).toString();
+  }
+
+  converter(){
+    this.valorEmReal = (parseFloat(this.dolar) * Math.round(parseFloat(this.real))).toString();
+  }
+
+  calcularMedia(){
+    this.calcularMediaRes = (((parseFloat(this.ac)*3) + (parseFloat(this.at)*5) + (parseFloat(this.agh)*2))/10).toString();
+  }
+
 }
